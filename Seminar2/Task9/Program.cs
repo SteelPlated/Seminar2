ac4fc101dee4e167d666bacae2446ff7bdcb0a28
SteelPlated <mix.singatulin@yandex.ru>
2022-05-18 20:16:01 +0500

    task9

---
+++ Seminar2/Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Случайное число
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"number={number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+Console.WriteLine($"number 1={firstDigit}");
+Console.WriteLine($"number 2={secondDigit}");
+
+if
+(firstDigit==secondDigit) Console.WriteLine($"Max= {firstDigit}");
+else
+Console.WriteLine($"Max= {secondDigit}");
+// Console.WriteLine(firstDigit > secondDigit ? $"Max= {firstDigit}" : $"Max = {secondDigit}");
