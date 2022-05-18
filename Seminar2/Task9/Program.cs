67204cea40bf43988c610905f57cb49055d76e80
SteelPlated <mix.singatulin@yandex.ru>
2022-05-18 20:33:47 +0500

    task9

--- Seminar2/Task9/Program.cs
+++ Seminar2/Task9/Program.cs
@@ -1,14 +1,30 @@
 ﻿// Случайное число
 
+//int number = new Random().Next(10, 100);
+//Console.WriteLine($"number={number}");
+//int firstDigit = number / 10;
+//int secondDigit = number % 10;
+//Console.WriteLine($"number 1={firstDigit}");
+//Console.WriteLine($"number 2={secondDigit}");
+
+// первый метод
+//if (firstDigit==secondDigit) 
+//Console.WriteLine($"Max= {firstDigit}");
+//else
+//Console.WriteLine($"Max= {secondDigit}");
+
+//второй метод
+// Console.WriteLine(firstDigit > secondDigit ? $"Max= {firstDigit}" : $"Max = {secondDigit}");
+
+//третий метод
 int number = new Random().Next(10, 100);
 Console.WriteLine($"number={number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-Console.WriteLine($"number 1={firstDigit}");
-Console.WriteLine($"number 2={secondDigit}");
+int maxNum = FindMaxNumber(number);
+Console.WriteLine($"Max={maxNum}");
 
-if
-(firstDigit==secondDigit) Console.WriteLine($"Max= {firstDigit}");
-else
-Console.WriteLine($"Max= {secondDigit}");
-// Console.WriteLine(firstDigit > secondDigit ? $"Max= {firstDigit}" : $"Max = {secondDigit}");
+int FindMaxNumber(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+return firstDigit / 10 > secondDigit % 10 ? firstDigit : secondDigit;
+}
